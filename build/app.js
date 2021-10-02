fb6a526f1ebfcb97328eef7a47bf1982e536eaea
CesarCr300 <71612510+CesarCr300@users.noreply.github.com>
2021-10-01 19:20:40 -0500

    Update app.js

--- build/app.js
+++ build/app.js
@@ -24,6 +24,6 @@ app.use(_express.default.urlencoded({
 
 app.use("/api/notes", _routes.default);
 app.use("/api/user", _routes2.default);
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {res.send("Working")});
 var _default = app;
 exports.default = _default;
